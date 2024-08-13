765efd7b2910e67d35329a5716a162a323d4784e
ManiTulasiRam <manitulasiramkamineni@gmail.com>
2024-08-13 15:52:21 +0100

    first commit

---
+++ config/dbConfig.js
@@ -0,0 +1,17 @@
+import mysql from "mysql";
+
+export const db = mysql.createPool({
+    connectionLimit: 100,
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'chef_zone'
+})
+
+
+
+
+
+
+
